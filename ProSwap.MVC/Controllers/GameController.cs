557ef50644520113a941a344010e195da6976f81
Justin Scroggins <justinscroggins@outlook.com>
2020-07-14 13:45:17 -0400

    Fixed Game Controller, tested create; working with WebAPI DB

--- ProSwap.MVC/Controllers/GameController.cs
+++ ProSwap.MVC/Controllers/GameController.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Web;
 using System.Web.Mvc;
 using ProSwap.Data;
@@ -21,7 +22,31 @@ namespace ProSwap.MVC.Controllers
             return View(_db.Games.ToList());
         }
 
+        // GET: Game/Details/{id}
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Game game = _db.Games.Find(id);
+            if (game == null)
+            {
+                return HttpNotFound();
+            }
+            return View(game);
+        }
+
+        // GET: Game/Create
+        public ActionResult Create()
+        {
+            ViewBag.ID = new SelectList(_db.Games, "GameId", "FirstName");
+            return View();
+        }
+
         // POST : Game/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
         public ActionResult Create(Game game)
         {
             if (ModelState.IsValid)
