e57ebf0d50a4a5572eaabd152f2e6f7c431a0aed
Justin Scroggins <justinscroggins@outlook.com>
2020-07-14 12:50:16 -0400

    Created game controller and setup game views for index and create

--- ProSwap.MVC/Controllers/GameController.cs
+++ ProSwap.MVC/Controllers/GameController.cs
@@ -3,15 +3,37 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using ProSwap.Data;
+using ProSwap.Models;
+using ProSwap.Services;
 
 namespace ProSwap.MVC.Controllers
 {
     public class GameController : Controller
     {
-        // GET: Game
+        private ApplicationDbContext _db = new ApplicationDbContext();
+        private GameService _gameService;
+        private string _userId;
+
+        // GET: Games
         public ActionResult Index()
         {
-            return View();
+            return View(_db.Games.ToList());
         }
+
+        // POST : Game/Create
+        public ActionResult Create(Game game)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Games.Add(game);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(game);
+        }
+
+
+
     }
 }
